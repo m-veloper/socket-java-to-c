824b4300da7f9eef5d68bda9750918d921995b88
min1474 <wkdals1474@naver.com>
2021-03-10 11:15:11 +0900

    no message

--- src/test/java/com/socket/javatoc/JavatocApplicationTests.java
+++ src/test/java/com/socket/javatoc/JavatocApplicationTests.java
@@ -1,13 +1,13 @@
-package com.socket.javatoc;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JavatocApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.socket.javatoc;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JavatocApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
